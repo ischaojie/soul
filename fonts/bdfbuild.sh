e36fb18504c19f47b4d2bebe8345d46fe352a041
shiniao <zhuzhezhe95@gmail.com>
2021-02-03 14:51:20 +0800

    📡: add chinese fonts support(冬青黑体)

---
+++ fonts/bdfbuild.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+./otf2bdf -r 200 -p 9 fonts/dqhtjw3.otf -o bdf/dqhtjw3_9pt.bdf
+./otf2bdf -r 200 -p 12 fonts/dqhtjw3.otf -o bdf/dqhtjw3_12pt.bdf
+./otf2bdf -r 200 -p 14 fonts/dqhtjw3.otf -o bdf/dqhtjw3_14pt.bdf
+./otf2bdf -r 200 -p 16 fonts/dqhtjw3.otf -o bdf/dqhtjw3_16pt.bdf
+./otf2bdf -r 200 -p 18 fonts/dqhtjw3.otf -o bdf/dqhtjw3_18pt.bdf
+
+./otf2bdf -r 200 -p 20 fonts/dqhtjw6.otf -o bdf/dqhtjw6_20pt.bdf
+./otf2bdf -r 200 -p 120 fonts/dqhtjw6.otf -o bdf/dqhtjw6_120pt.bdf
+
---
+++ fonts/build.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./bdfconv bdf/dqhtjw3_9pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw3_9pt_gb2312 -o release/u8g2_font_dqhtjw3_9pt_gb2312.c
+./bdfconv bdf/dqhtjw3_12pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw3_12pt_gb2312 -o release/u8g2_font_dqhtjw3_12pt_gb2312.c
+./bdfconv bdf/dqhtjw3_14pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw3_14pt_gb2312 -o release/u8g2_font_dqhtjw3_14pt_gb2312.c
+./bdfconv bdf/dqhtjw3_16pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw3_16pt_gb2312 -o release/u8g2_font_dqhtjw3_16pt_gb2312.c
+./bdfconv bdf/dqhtjw3_18pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw3_18pt_gb2312 -o release/u8g2_font_dqhtjw3_18pt_gb2312.c
+./bdfconv bdf/dqhtjw6_20pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw6_20pt_gb2312 -o release/u8g2_font_dqhtjw3_20pt_gb2312.c
+./bdfconv bdf/dqhtjw6_120pt.bdf -b 0 -f 1 -M maps/gb2312.map -n u8g2_font_dqhtjw6_120pt_gb2312 -o release/u8g2_font_dqhtjw3_120pt_gb2312.c
